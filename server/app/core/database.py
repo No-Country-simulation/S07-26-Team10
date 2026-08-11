7356c963d384137bce67e0f45c6c57e5bcdc70ec
anhonybanion <anthonybaonarias@my.uopeople.edu>
2026-08-11 14:24:04 -0300

    cambio de base de datos

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -12,6 +12,8 @@ class Base(DeclarativeBase):
 engine = create_engine(
     settings.DATABASE_URL,
     echo=settings.DEBUG,
+    pool_pre_ping=True,
+    pool_recycle=300,
 )
 
 SessionLocal = sessionmaker(
